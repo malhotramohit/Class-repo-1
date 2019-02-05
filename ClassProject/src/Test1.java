4f08e32725956188f7c8995799c921f2d7371349
mohitmalhotra <mohitmalhotra@192.168.1.31>
2019-02-05 09:40:25 +0530

    First commit for class and object project

---
+++ ClassProject/src/Test1.java
@@ -0,0 +1,58 @@
+
+public class Test1 {
+	public static void main(String[] args) {
+		
+		// int a= 2
+		// a=3
+		// int[] arr = new int[3];
+		// arr[0]=2
+		// arr[1]
+		
+		// classname  oldLamp  = new classname();
+		// create object
+		Lamp oldLamp = new Lamp(); // 010100
+		
+		Lamp newLamp = new Lamp(); // 9092990
+		
+		
+		//Emp emp1 = new Lamp();
+		
+		//Emp emp1 = new Emp();
+		
+		// membership oper=== "." == dot op
+		
+		System.out.println(oldLamp.isOn);
+		
+		oldLamp.switchOn();
+		
+		System.out.println(oldLamp.isOn);
+		
+		System.out.println("isOn flag for new Lamp " +newLamp.isOn);
+		
+		
+		
+		
+		
+		
+		
+	}
+}
+
+// class declare
+class Lamp {
+	
+	// member variable , field
+	boolean isOn = false;	
+	int id = 0;
+	String nameOfLamp = "MyLamp";
+	
+	// methods : method name, return type void , int, short , int[], Lamp, body { }
+	void switchOn() {
+		System.out.println("inside swicth in method");
+		isOn = true;
+		System.out.println("exit swicth  method");
+	}
+	
+	
+	
+}
